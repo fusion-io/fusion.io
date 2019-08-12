ae7070214ac553a264316087e18cfc0f00bfd173
rikky <son.levuthai@gmail.com>
2019-08-13 00:55:03 +0700

    adding environment check when running migration

--- packages/knex/src/commands/migration/Migration.ts
+++ packages/knex/src/commands/migration/Migration.ts
@@ -14,6 +14,27 @@ export default class Migration extends Command {
 
     subCommands = [ Create, Up, Down, Init ];
 
+    options = {
+        force: {
+            describe: 'Force run the migration',
+            alias: 'f',
+            type: 'boolean',
+            default: false
+        }
+    };
+
+    middlewares = [(argv: any) => this.checkForEnvironment(argv)];
+
+    async checkForEnvironment(argv: any) {
+        if ('production' === process.env.NODE_ENV && (!argv.force)) {
+            await this.output.error('box', 'You are about running a migration command in production environment.' +
+                ' This action might cause data loss.\nIf you understand the risks and wish to continue,' +
+                ' re-run the command with the --force flag. Exiting command with error code 20');
+
+            process.exit(20);
+        }
+    }
+
     @inject(DatabaseMigrator)
     protected async execute(argv: any, migrator: DatabaseMigrator) {
         await this.output.operate('table', { head: ['', '#', 'migration' ]}, async (table: any) => {
