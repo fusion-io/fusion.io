00c9529ff1c7d64932a2f1be95a0b90eb84773af
rikky <son.levuthai@gmail.com>
2019-08-13 01:11:04 +0700

    More interactive output

--- packages/knex/src/commands/migration/Init.ts
+++ packages/knex/src/commands/migration/Init.ts
@@ -1,12 +1,19 @@
 import { Command } from "@fusion.io/commands";
-import {inject, singleton} from "@fusion.io/core";
+import { inject, singleton } from "@fusion.io/core";
 import DatabaseMigrator from "../../migration/DatabaseMigrator";
+import chalk from "chalk";
 
 @singleton()
 export default class Init extends Command {
 
-    @inject(DatabaseMigrator)
+    describe = 'Initialize the database migration table.';
+
+    @inject(DatabaseMigrator, 'config')
     protected async execute(argv: any, migrator: DatabaseMigrator) {
         await migrator.state.prepare();
+        this.output.info(
+            'log',
+            `Initialized migration table [${chalk.yellow((migrator.state as any).table)}]`
+        );
     }
 }
