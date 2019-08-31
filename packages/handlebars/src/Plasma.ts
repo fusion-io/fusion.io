4c427914c29890943db66c830f979b544f783db8
rikky <son.levuthai@gmail.com>
2019-08-31 23:17:18 +0700

    remove the helpers, it having too many dependencies

--- packages/handlebars/src/Plasma.ts
+++ packages/handlebars/src/Plasma.ts
@@ -1,13 +1,10 @@
 import { inject, Plasma as CorePlasma } from "@fusion.io/core";
 import Environment from "./Environment";
-import Handlebars from "handlebars";
 
 export default class Plasma extends CorePlasma {
 
     @inject(Environment)
     compose(env: Environment) {
-        Handlebars.registerHelper(require('handlebars-helpers')());
-
         env.loadViews(this.config.view.directory);
     }
 }
