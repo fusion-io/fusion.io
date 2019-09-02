2c50c476ab436fd02a99c5100dca44ae98a6a38b
rikky <son.levuthai@gmail.com>
2019-09-02 22:40:31 +0700

    correct route listing command when route does not have name

--- packages/proton/src/commands/Routes.ts
+++ packages/proton/src/commands/Routes.ts
@@ -12,7 +12,7 @@ export default class Routes extends Command {
     @inject(Router)
     protected async execute(argv: any, router: Router) {
         const routes = router.stack.filter((layer: Layer) => layer.opts.end).map((layer: Layer, index) =>
-            [ index + 1, layer.methods.join(','), layer.path, layer.name ].map(i => chalk.cyan(i.toString()))
+            [ index + 1, layer.methods.join(','), layer.path, layer.name ].map(i => chalk.cyan(i ? i.toString() : ''))
         );
 
         await this.output.operate('table', { head: [ '#', 'method', 'url', 'name'] }, routes)
